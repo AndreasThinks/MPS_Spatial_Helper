30bdc2a6a50eb6849434bcd79f13b96c28e5d522
Andreas Varotsis <andreas.varotsis@met.police.uk>
2021-09-01 16:58:50 +0100

    setup.py fixes

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='mps_spatial_helper',
     version='0.1.0',
-    packages=["geopandas",
-              "h3",
-              "pandas",
-              "folium"],
+    packages=find_packages(),
     url='https://github.com/Data-Science-Policing-Security/MPS_Spatial_Helper',
     license='GPL-3.0 License',
     author='Andreas Varotsis',
     author_email='andreas.varotsis@met.police.uk',
-    description='A collection of code designed to help ingestion, manipulation and visualisation of MPS spatial data'
+    description='A collection of code designed to help ingestion, manipulation and visualisation of MPS spatial data',
+    install_requires=[
+        "geopandas",
+        "folium",
+        "h3"],
 )
